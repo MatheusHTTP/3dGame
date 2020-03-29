212cbd8229a903a4251b3fd4e89ee4af8bd2c762
Matheus Soares <matheus.http@hotmail.com>
2020-03-29 19:29:52 -0300

    update

---
+++ Assets/explosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public float bombForce = 2000;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+            Explosion();
+    }
+    // Update is called once per frame
+    void Explosion()
+    {
+        Destroy(gameObject, 1);
+        RaycastHit[] hits;
+
+        hits = Physics.SphereCastAll(transform.position, 5, Vector3.up, 10);
+
+        if(hits.Length > 0) { 
+            foreach(RaycastHit hit in hits) {
+                if (hit.rigidbody) {
+                    hit.rigidbody.isKinematic = false;
+                    hit.rigidbody.AddExplosionForce(bombForce, transform.position, 10);
+                }
+            
+            }
+
+        }
+    }
+}
